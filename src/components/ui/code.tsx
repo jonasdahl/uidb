d26ea5f58884e6f024444d5c46d08a6a192c3a68
Jonas Dahl <jonas@jdahl.se>
2023-08-28 17:51:10 +0200

    add interactive test

--- src/components/ui/code.tsx
+++ src/components/ui/code.tsx
@@ -6,7 +6,9 @@ export function Code({ children }: { children: ReactNode }) {
       className="text-xs border border-solid border-neutral-web-unifi-color-neutral-02 bg-neutral-web-unifi-color-neutral-01 rounded p-4 overflow-auto"
       style={{ maxHeight: "50vh" }}
     >
-      <code className="whitespace-pre">{children}</code>
+      <code className="whitespace-pre" role="code">
+        {children}
+      </code>
     </div>
   );
 }
--- src/routes/devices.$deviceId/_route.test.tsx
+++ src/routes/devices.$deviceId/_route.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
@@ -32,6 +33,46 @@ describe("device route", () => {
     // Expect the name and ID to be in the document
     expect(await screen.findAllByText("Unifi Test Product")).toBeTruthy();
     expect(await screen.getByText("device-id-1")).toBeTruthy();
+
+    // Stop server
+    server.close();
+  });
+
+  it("shows raw json when button is clicked", async () => {
+    // Set up a mocked device as response from the server
+    const mockedResponse: Uidb = {
+      version: "96891c4f-6940-4475-9c4d-15de1863bf1c",
+      devices: [{ id: "device-id-1", product: { name: "Unifi Test Product" } }],
+    };
+
+    // Start the server
+    const server = setupServer(
+      rest.get(
+        "https://static.ui.com/fingerprint/ui/public.json",
+        (_req, res, ctx) => res(ctx.json(mockedResponse))
+      )
+    );
+    server.listen();
+
+    // Add the route to the mocked router and render it
+    const router = createMemoryRouter(
+      [{ path: "/devices/:deviceId", lazy: () => import("./_route") }],
+      { initialEntries: ["/devices/device-id-1"] }
+    );
+    render(<RouterProvider router={router} />);
+
+    expect(await screen.findAllByText("Unifi Test Product")).toBeTruthy();
+
+    const button = await screen.getByRole("button", {
+      name: "See All Details as JSON",
+    });
+    userEvent.click(button);
+
+    const codeElement = await screen.findByRole("code");
+    expect(JSON.parse(codeElement.textContent!)).toEqual({
+      id: "device-id-1",
+      product: { name: "Unifi Test Product" },
+    });
 
     // Stop server
     server.close();
